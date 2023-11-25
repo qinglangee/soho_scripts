2cdcbe1b2c84c161690cbe935793ec7f9ce58f5c
zhch <zhchqingdao@163.com>
2023-11-25 13:12:36 +0800

    tishi 命令更新

--- functions/tishi.py
+++ functions/tishi.py
@@ -1,10 +1,13 @@
 from collections import OrderedDict
+import clipboard
 
 names = OrderedDict({
     'git':[
         'git archive --format zip --output /path_out.zip master',
         'git update-index --assume-unchanged filename',
         'git update-index --no-assume-unchanged filename',
+        'git config user.name "name"',
+        'git config user.email "email"',
     ],
     'php':{
         'php -S localhost:8000',
@@ -21,16 +24,34 @@ names = OrderedDict({
         'Get-FileHash ".\\2021-04-18 21-18-28.mkv" -Algorithm md5',
         'Get-FileHash ".\\2021-04-18 21-18-28.mkv" -Algorithm sha256'
     ],
+    'gradle':[
+        'gradle test',
+        'gradle checkstyleMain'
+    ],
+    'python':[
+        'python -m venv  myvenv',
+        'myvenv/Scripts/activate.ps1',
+        'pip install -r requirements.txt',
+    ],
+    # bash 可能会比较多，可以多分几组
+    'bash lslibs':[],
+    'lslibs':[
+        'dir=target/dependency/;aa="";for f in `ls $dir`;do aa="$aa;$dir$f"; done; echo $aa'
+    ],
+    # java
+    'java uml': [],
+    'uml':['java -cp "classes;urm-core.jar;target\dependency\*"  com.iluwatar.urm.DomainMapperCli -p "com.pkg,com.aaa" -f class.puml -i "com.abc,com.def"']
+
 })
 
 
 def short_desc():
     hint = "tishi 提示一些命令用法 \n"
-    hint += "    tishi git \n"
+    hint += "    tishi git 1\n"
     hint += "    tishi xcopy \n"
     return hint
 def print_help():
-    hint = "至少需要一个参数。 命令名字\n"
+    hint = "至少需要一个参数。 命令名字 <复制index>\n"
     hint += "tishi git\n"
     hint += "可以查看如下命令:\n"
     print(hint)
@@ -49,14 +70,21 @@ def check_input(args):
         print_help()
         return
     name = args[0]
-    show_cmd(name)
+    copy_index = 0  # 指定复制哪一个命令
+    if len(args) > 1:
+        copy_index = int(args[1])  # 第2个参数作为复制的索引
+    show_cmd(name, copy_index)
 
-def show_cmd(name):
+def show_cmd(name, copy_index=0):
     if name in names:
         items = names[name]
         print(name,":")
+        index = 0
         for item in items:
-            print("\t", item)
+            print("\t", str(index) + '.', item)
+            if copy_index==index:
+                clipboard.copy(item)
+            index += 1
     else:
         print_help()
     
